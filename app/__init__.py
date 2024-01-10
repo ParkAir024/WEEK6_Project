458f28495fa057e2afda751e4c6bafe58f149d7c
ParkAir024 <pcreidy24@gmail.com>
2024-01-10 02:30:14 -0600

    6.75

--- app/__init__.py
+++ app/__init__.py
@@ -4,7 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_jwt_extended import JWTManager
 
-from Config import Config
+from config import Config
 
 app = Flask(__name__)
 app.config.from_object(Config)
