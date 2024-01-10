c2e8360fcc371a097696444e4fc14a749c250fce
ParkAir024 <pcreidy24@gmail.com>
2024-01-10 02:53:00 -0600

    6.80

--- Resources/User/__init__.py
+++ Resources/User/__init__.py
@@ -2,4 +2,4 @@ from flask_smorest import Blueprint
 
 bp = Blueprint('users', __name__, description="Operations for Users")
 
-from . import routes,auth_routes
+from . import routes, auth_routes
--- app/__init__.py
+++ app/__init__.py
@@ -20,5 +20,5 @@ from models import AnimeModel, UserModel
 from resources.user import bp as user_bp
 api.register_blueprint(user_bp)
 
-from resources.anime import bp as post_bp
-api.register_blueprint(post_bp)
+from resources.anime import bp as anime_bp
+api.register_blueprint(anime_bp)
